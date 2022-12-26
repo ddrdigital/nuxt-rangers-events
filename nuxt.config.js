53fb86edd6f1141a7f9b8cb0915461a35a27f34d
Darryl Dolan-Rathmell <dobby@Dobbys-MacBook-Pro.local>
2022-12-26 19:04:48 +0000

    Project name changed.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-ranger-events',
+    title: 'Nuxt Ranger Events',
     htmlAttrs: {
       lang: 'en'
     },
