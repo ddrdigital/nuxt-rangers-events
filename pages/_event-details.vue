c6f45f2e63fa14a3e600362285cd43a3fefc53d1
Darryl Dolan-Rathmell <dobby@Dobbys-MacBook-Pro.local>
2023-01-12 17:06:52 +0000

    Migration step, going from Vue 2 to Nuxt.

---
+++ pages/_event-details.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Details: {{ slug }}</h1>
+</template>
+  
+<script>
+export default {
+    name: 'RangerEvents',
+    async asyncData({ params }) {
+      const slug = params.slug // When calling /abc the slug will be "abc"
+      return { slug }
+    }
+}
+</script>
